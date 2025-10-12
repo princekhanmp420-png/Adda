8ed66b737fe283d5fa4d89ab41ff7cec0af78375
Prince Morshed <princechowdhury862478@gmail.com>
2025-10-12 20:02:49 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -16,7 +16,7 @@ module.exports = {
 			session4: "tối",
 			leaveType1: "tự rời",
 			leaveType2: "bị kick",
-			defaultLeaveMessage: "{userName} đã {type} khỏi nhóm"
+			defaultLeaveMessage: "{userName} তোমারে সম্মানের সহিত {type} গ্রুপ থেকে রিমুভ করা হলো"
 		},
 		en: {
 			session1: "morning",
@@ -25,7 +25,7 @@ module.exports = {
 			session4: "evening",
 			leaveType1: "left",
 			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
+			defaultLeaveMessage: "কিরে {userName} {type} তোর কি সমস্যা আমার বস প্রিন্স রে না বলে লিভ নিবি না"
 		}
 	},
 
